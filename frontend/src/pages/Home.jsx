d6289876f43b2465ffdf47d937e033c14b656b0d
mattslaney <mattslaney@users.noreply.github.com>
2023-04-06 18:33:17 +0100

    update key on card

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,7 +3,7 @@ import { Loader, Card, FormField } from '../components';
 
 const RenderCards = ({ data, title }) => {
   if (data?.length > 0)
-    return data.map((post) => <Card key={post.id} {...post} />);
+    return data.map((post) => <Card key={post._id} {...post} />);
 
   return (
     <h2 className="mt-5 font-bold text-[#6449ff] text-xl uppercase">{title}</h2>
